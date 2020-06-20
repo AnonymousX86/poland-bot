6c4b86d1b3f0b303ca5cf343e80aae344be30075
AnonymousX86 <53279236+AnonymousX86@users.noreply.github.com>
2020-06-20 21:55:54 +0200

    Added "wybierz" and "moneta" comamnds

--- GameMaster/cogs/losowe.py
+++ GameMaster/cogs/losowe.py
@@ -6,7 +6,7 @@ from discord.ext.commands import Cog, command
 
 from GameMaster.templates.basic import error_em
 from GameMaster.templates.other import member_em
-from GameMaster.templates.random import dice_em
+from GameMaster.templates.random import dice_em, choose_em, eight_ball_em
 
 
 class Losowe(Cog):
@@ -80,6 +80,60 @@ class Losowe(Cog):
                 await ctx.send(embed=error_em(f'Nie mogę znaleźć nikogo z rolą {role.mention}.'))
         else:
             await ctx.send(embed=error_em('Nie ma takiej roli.'))
+
+    @command(
+        name='wybierz',
+        brief='Pomaga w wyborze.',
+        description='Wybiera spomiędzy wprowadzonych rzeczy.',
+        help='Oddzielaj pozycje przecinkami.',
+        usage='[coś 1], [coś 2], ... [coś n]'
+    )
+    async def wybierz(self, ctx, *, things=None):
+        if not things:
+            await ctx.send(embed=error_em('Nie podałeś nic.'))
+        else:
+            things = things.split(', ')
+            if len(things) < 2:
+                await ctx.send(embed=error_em('Podaj co najmniej 2 rzeczy.'))
+            else:
+                await ctx.send(embed=choose_em(':arrow_right:', choice(things)))
+
+    @command(
+        name='moneta',
+        brief='Rzuca monetą.'
+    )
+    async def moneta(self, ctx):
+        lot = randint(1, 1000)
+        if lot <= 1:
+            result = [':face_with_raised_eyebrow:', 'Moneta znikła']
+        elif lot <= 10:
+            result = [':exploding_head:', 'Moneta stanęła na krawędzi']
+        elif lot <= 505:
+            result = [':arrow_heading_down:', 'Orzeł']
+        else:
+            result = [':arrow_heading_up:', 'Reszka']
+        await ctx.send(embed=choose_em(result[0], result[1]))
+
+    @command(
+        name='8ball',
+        brief='Kula tobie pomoże.',
+        usage='<pytanie>'
+    )
+    async def eight_ball(self, ctx):
+        await ctx.send(embed=eight_ball_em(choice([
+            'Jasne że tak',
+            'Może lepiej nie',
+            'Chyba sobie żartujesz',
+            'Też nie wiem',
+            'Lepiej spytać sąsiada',
+            'Admin mówi że tak',
+            'Tak, ale Mikołajowi się to nie spodoba',
+            'Teoretycznie tak, a praktycznie nie',
+            'Trudne pytanie, muszę się chwilę zastanowić',
+            'A co za różnica',
+            'Nie, znaczy tak',
+            'Tak, ale nie chcę wiedzieć skąd to pytanie'
+        ])))
 
 
 def setup(bot):
