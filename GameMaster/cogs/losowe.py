9513fe3cd4bf8178740f34b2a11d079982802414
AnonymousX86 <53279236+AnonymousX86@users.noreply.github.com>
2020-06-20 19:18:22 +0200

    Added command "losowy"

--- GameMaster/cogs/losowe.py
+++ GameMaster/cogs/losowe.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from random import randint
+from random import randint, choice
 
 from discord import HTTPException
 from discord.ext.commands import Cog, command
@@ -42,6 +42,45 @@ class Losowe(Cog):
                     await ctx.send(embed=dice_em(result, sep))
                 except HTTPException:
                     await ctx.send(embed=error_em('Zbyt duże wartości!'))
+
+    @command(
+        name='losowy',
+        brief='Wybiera losowego użytkownika.',
+        description='Wyświetlane są nazwa, zdjęcie, status i nick losowego użytkownika z serwera.',
+        usage='[rola|ID roli]'
+    )
+    async def losowy(self, ctx, arg=None):
+        role_id = 0
+        member = None
+
+        if arg:
+            # Role ID only
+            if len(arg) == 18:
+                try:
+                    role_id = int(arg)
+                except ValueError:
+                    pass
+            # Role mention
+            elif len(arg) == 21:
+                role_id = int(arg[3:-1])
+            role = ctx.guild.get_role(role_id)
+            if not role:
+                # User mention
+                if len(arg) == 22:
+                    member = ctx.guild.get_member(int(arg[3:-1]))
+        else:
+            role = ctx.guild.default_role
+
+        if member:
+            await ctx.send(mebed=member_em(member))
+        elif role:
+            possibles = [member for member in ctx.guild.members if role in member.roles]
+            if possibles:
+                await ctx.send(embed=member_em(choice(possibles)).set_footer(text=f'Wylosowany(a) z grupy {role.name}.'))
+            else:
+                await ctx.send(embed=error_em(f'Nie mogę znaleźć nikogo z rolą {role.mention}.'))
+        else:
+            await ctx.send(embed=error_em('Nie ma takiej roli.'))
 
 
 def setup(bot):
