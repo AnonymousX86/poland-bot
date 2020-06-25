b8fd4111f275600c6c6382d8cd22b28c825a4319
AnonymousX86 <53279236+AnonymousX86@users.noreply.github.com>
2020-06-25 14:33:36 +0200

    Fixed syntax according
    All code should be inside `if __name__ == '__main__'` if not in functions.

--- GameMaster/__main__.py
+++ GameMaster/__main__.py
@@ -5,85 +5,80 @@ from os import getenv
 from discord import Status, Activity, ActivityType
 from discord.ext.commands import Bot, ExtensionNotFound, ExtensionAlreadyLoaded, NoEntryPointError, \
     MissingPermissions, BotMissingPermissions
-from nest_asyncio import apply
+from nest_asyncio import apply as async_apply
 
 from GameMaster.templates.basic import error_em
 from GameMaster.utils.database.basic import check_connection
-from settings import bot_token
-
-
-class CheckDatabaseError(BaseException):
-    pass
-
-
-apply()
-
-basicConfig(level=INFO)
-
-bot = Bot(
-    command_prefix='$',
-    case_insensitive=False,
-    description='Bot serwera Poland.',
-    owner_id=309270832683679745,
-)
-
-
-@bot.event
-async def on_ready():
-    print('Logged in as: {0} ({0.id})'.format(bot.user))
-
-    if not check_connection():
-        raise CheckDatabaseError('Can\'t check connection to PostgreSQL.')
-    else:
-        print('PostgreSQL connection OK')
-
-    status = Status.online
-    ac = 'Kości'
-    if getenv('LOCALAPPDATA') == 'C:\\Users\\Kubas\\AppData\\Local':
-        ac += ' (w produkcji)'
-    activity = Activity(type=ActivityType.playing, name=ac)
-    await bot.change_presence(status=status, activity=activity)
+from settings import get_bot_token
 
 
 async def error_msg(ctx, error):
     if isinstance(error, MissingPermissions):
-        status = 'Nieuprawnionym wstęp wzbroniony!'
+        s = 'Nieuprawnionym wstęp wzbroniony!'
 
     elif isinstance(error, BotMissingPermissions):
-        status = 'Nie posiadam uprawnień!'
+        s = 'Nie posiadam uprawnień!'
 
     else:
-        status = f'```py\n{error.__class__.__name__}: {error}```'
+        s = f'```py\n{error.__class__.__name__}: {error}```'
 
-    await ctx.send(embed=error_em(status))
+    await ctx.send(embed=error_em(s))
 
 
-bot.error_msg = error_msg
+if __name__ == '__main__':
+    async_apply()
+    basicConfig(level=INFO)
 
-cogs = [f'GameMaster.cogs.{cog}' for cog in [
-    'warny',
-    'czyszczenie',
-    'losowe',
-    'podstawowe',
-    'zabawne'
-]]
+    bot = Bot(
+        command_prefix='$',
+        case_insensitive=False,
+        description='Bot serwera Poland.',
+        owner_id=309270832683679745,
+    )
 
-for cog in cogs:
-    p = '[COGS]'
-    try:
-        bot.load_extension(cog)
-        print(f'{p} Loaded: {cog}')
-    except ExtensionNotFound:
-        print(f'{p} Not found: {cog}')
-    except ExtensionAlreadyLoaded:
-        print(f'{p} Already loaded: {cog}')
-    except NoEntryPointError:
-        print(f'{p} Cog "{cog}" do not have "setup()" function')
-    except Exception as e:
-        print(f'{p} {e.__class__.__name__}: {e}')
 
-bot.history = {
-    'last_8ball': ''
-}
+    @bot.event
+    async def on_ready():
+        print('[Bot] Logged in as: {0} ({0.id})'.format(bot.user))
 
-bot.run(bot_token())
+        p = '[Database]'
+        if check_connection():
+            print(f'{p} Connection OK')
+        else:
+            print(f'{p} Can\'t connect')
+
+        status = Status.online
+        ac = 'Kości'
+        if getenv('LOCALAPPDATA') == 'C:\\Users\\Kubas\\AppData\\Local':
+            ac += ' (w produkcji)'
+        activity = Activity(type=ActivityType.playing, name=ac)
+        await bot.change_presence(status=status, activity=activity)
+
+        bot.error_msg = error_msg
+
+        for cog in [f'GameMaster.cogs.{cog}' for cog in [
+            'warny',
+            'czyszczenie',
+            'losowe',
+            'podstawowe',
+            'zabawne'
+        ]]:
+            p = '[Cogs]'
+            try:
+                bot.load_extension(cog)
+                print(f'{p} Loaded: {cog}')
+            except ExtensionNotFound:
+                print(f'{p} Not found: {cog}')
+            except ExtensionAlreadyLoaded:
+                print(f'{p} Already loaded: {cog}')
+            except NoEntryPointError:
+                print(f'{p} Extension "{cog}" do not have "setup()" function')
+            except Exception as e:
+                print(f'{p} {e.__class__.__name__}: {e}')
+
+        bot.history = {
+            'last_8ball': ''
+        }
+
+
+    bot.run(get_bot_token())
