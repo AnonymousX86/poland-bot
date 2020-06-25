dc6ec02aa2c832d34b45b0a2f1cf0d3c8c03f3f8
AnonymousX86 <53279236+AnonymousX86@users.noreply.github.com>
2020-06-25 11:20:14 +0200

    Moved "8ball" to another cog
    Created "zabawne" cog.
    Fixed syntax and language in "8ball" command.
    Added "rate" command.

---
+++ GameMaster/cogs/zabawne.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+from random import seed, randint, choice
+
+from discord.ext.commands import Cog, command
+
+from GameMaster.templates.basic import error_em
+from GameMaster.templates.random import eight_ball_em, rate_em
+
+
+class Zabawne(Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @command(
+        name='8ball',
+        brief='Kula tobie pomoże.',
+        help='Pytania powinny zaczynać się od "czy", ponieważ oryginalna kula odpowiada na pytania:'
+             ' "Czy powinienem (...) ?".',
+        usage='<pytanie>'
+    )
+    async def eight_ball(self, ctx, *, arg=None):
+        if not arg:
+            await ctx.send(embed=error_em('Nie podałeś/aś pytania'))
+        elif not arg.endswith('?'):
+            await ctx.send(embed=error_em('Pytania kończą się na `?`'))
+        else:
+            while True:
+                result = choice([
+                    'Jasne że tak',
+                    'Może lepiej nie',
+                    'Chyba sobie żartujesz',
+                    'Też nie wiem',
+                    'Lepiej spytać sąsiada',
+                    'Admin mówi że tak',
+                    'Tak, ale Mikołajowi się to nie spodoba',
+                    'Teoretycznie tak, a praktycznie nie',
+                    'Trudne pytanie, muszę się chwilę zastanowić',
+                    'A co za różnica',
+                    'Nie, znaczy tak',
+                    'Tak, ale nie chcę wiedzieć skąd to pytanie',
+                    'A nie możesz samemu sobie odpowiedzieć',
+                    'Proszę pomóż mi, jestem uwięziony w środku bota',
+                    'Nie rozumiem',
+                    'Spróbuj jeszcze raz',
+                    'Pomyśl',
+                    'Idę coś zjeść, będę za 15 minut',
+                    'Nie mi oceniać',
+                    'Ja też potrzebuję snu',
+                    'Proszę przestań się mnie pytać o takie głupoty',
+                    'Wciśnij Alt + F4',
+                    'Jednorożce, tak to wszystko',
+                    'Chyba nie myślisz, że serio pomogę',
+                    'A co zrobiłby Jedi',
+                    'To morderca',
+                    'Mój kontakt mówi, że nie, ale również mówi, że Duda wygra',
+                    'Żartujesz... Prawda?',
+                    'Lepiej zapytaj się mnie, czy mnie to obchodzi',
+                    'Na 100%',
+                    'Tak, ale zrób to będąc nawalonym jak tylko potrafisz',
+                    'Nie mogę teraz powiedzieć',
+                    'Zależy',
+                    'To nie jest OK',
+                    'Odpowiedź znajdziesz po swojej prawej stronie'
+                ])
+                if result != self.bot.history['last_8ball']:
+                    self.bot.history['last_8ball'] = result
+                    break
+            await ctx.send(embed=eight_ball_em(result))
+
+    @command(
+        name='oceń',
+        brief='Wystawia ocenę.',
+        description='Wystawia ocenę z zakresu od 1 do 10.',
+        help='Ten sam przedmiot zawsze otrzyma taką samą ocenę.',
+        usage='<przedmiot>',
+        aliases=['rate']
+    )
+    async def rate(self, ctx, *, arg=None):
+        if not arg:
+            await ctx.send(embed=error_em('Musisz podać rzecz do ocenienia!'))
+        else:
+            arg = arg.lower()
+            seed(arg)
+            ocena = randint(1, 10)
+            await ctx.send(embed=rate_em(arg, ocena))
+
+
+def setup(bot):
+    bot.add_cog(Zabawne(bot))
