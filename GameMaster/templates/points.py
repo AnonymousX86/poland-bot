bd72649614f88153dc82a3cad3c6bf99c39c7149
AnonymousX86 <53279236+AnonymousX86@users.noreply.github.com>
2020-07-03 14:46:53 +0200

    Moved embed to "templates/users.py"

--- GameMaster/templates/points.py
+++ GameMaster/templates/points.py
@@ -1,27 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from discord import Embed, Member, Color
+from discord import Embed, Color
 
 from GameMaster.utils.database.basic import now
-from GameMaster.utils.database.points import points_to_level
-
-
-def profile_em(member: Member, points):
-    level = points_to_level(points)
-    level = level['level_id'] if level else 0
-    return Embed(
-        title=member.display_name,
-        description='\u200b',
-        color=Color.blue()
-    ).add_field(
-        name='Punkty',
-        value=points
-    ).add_field(
-        name='Poziom',
-        value=level
-    ).set_thumbnail(
-        url=member.avatar_url
-    )
 
 
 def ranking_em(ranking: str):
