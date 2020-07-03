197b4e941efb96a256dca5d9f3ed05be043f5975
AnonymousX86 <53279236+AnonymousX86@users.noreply.github.com>
2020-07-03 14:41:57 +0200

    Create ranking.py

---
+++ GameMaster/utils/ranking.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+def nth_place(place: int) -> str:
+    if place == 1:
+        return ':first_place:'
+    elif place == 2:
+        return ':second_place:'
+    elif place == 3:
+        return ':third_place:'
+    else:
+        return str(place)
