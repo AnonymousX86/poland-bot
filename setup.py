95ca7931742051188e4d4c73e8f78c8890cb5350
AnonymousX86 <53279236+AnonymousX86@users.noreply.github.com>
2020-07-04 23:44:05 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Poland Bot (Game Master)',
+    version='1.0.0',
+    packages=['GameMaster', 'GameMaster.cogs', 'GameMaster.test', 'GameMaster.utils', 'GameMaster.utils.database',
+              'GameMaster.templates'],
+    url='https://github.com/AnonymousX86/poland-bot',
+    license='MIT License',
+    author='Jakub Suchenek',
+    author_email='jakub.suchenek.25@gmail.com',
+    description='Discord bot for Poland server'
+)
