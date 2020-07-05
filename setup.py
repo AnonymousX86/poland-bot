bd2512b92fcd85ef6fd6fd89d4d104fca943b79c
AnonymousX86 <53279236+AnonymousX86@users.noreply.github.com>
2020-07-05 11:47:30 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Poland Bot (Game Master)',
-    version='1.0.0',
+    version='1.1.0',
     packages=['GameMaster', 'GameMaster.cogs', 'GameMaster.test', 'GameMaster.utils', 'GameMaster.utils.database',
               'GameMaster.templates'],
     url='https://github.com/AnonymousX86/poland-bot',
